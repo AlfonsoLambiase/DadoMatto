3ae73339b5cbcb0dd6649326072258758c0f4bb3
natasciarufino <natasciarufino17@gmail.com>
2025-05-20 16:03:17 +0200

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import {Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './Pages/Home';
-import Profilo from './Pages/Profilo';
-import Regole from './Pages/Regole';
-import Gioco from './Pages/Gioco';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./Pages/Home";
+import Contatti from "./Pages/Contatti";
+import FAQ from "./Pages/FAQ";
+import SocialMedia from "./Pages/SocialMedia";
+import Profilo from "./Pages/Profilo";
+import Regole from "./Pages/Regole";
+import Gioco from "./Pages/Gioco";
 
 function App() {
   return (
-    <>
-    <Header />
-    <Home/>
-    </>
-  )
-};
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contatti" element={<Contatti />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/SocialMedia" element={<SocialMedia />} />
+          <Route path="/Profilo" element={<Profilo />} />
+          <Route path="/Regole" element={<Regole />} />
+          <Route path="/Gioco" element={<Gioco />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
