037347bc9fdbf8f3e4be372da0e6ea6c64dcd1e0
Alfonso Lambiase <lambiase1199@gmail.com>
2025-05-23 23:21:44 +0200

    Responsive App

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()],
+  base: '/DadoMatto/', // 👈 necessario per GitHub Pages
+  plugins: [react(), tailwindcss()]
 })
